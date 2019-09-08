ea860a3f6e01b578aca2dbf6fcd8a369f832e652
Marco Torello <marco.torello@dlodh.com>
2019-09-08 23:13:04 +0200

    Add multiple variable but missing doc

--- win-adfs-website-config/win_adfs_website_config.ps1
+++ win-adfs-website-config/win_adfs_website_config.ps1
@@ -6,21 +6,26 @@
 #Requires -Module Ansible.ModuleUtils.Legacy
 
 $ErrorActionPreference = "Stop"
+$skewRange = 0..15
 
 $params = Parse-Args -arguments $args -supports_check_mode $true
 $checkMode      = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
 $diffMode       = Get-AnsibleParam -obj $params -name "_ansible_diff" -type "bool" -default $false
 
-$name               = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
-$state              = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
-$endpoints          = Get-AnsibleParam -obj $params -name "endpoints" -type "list" -failifempty $true
-$claimsrules        = Get-AnsibleParam -obj $params -name "claimsrules" -type "str" -failifempty $true
-$oauthClientType    = Get-AnsibleParam -obj $params -name "oauth_clients_type" -type "str" -default "Public" -validateSet "public", "confidential"
-$scopes             = Get-AnsibleParam -obj $params -name "oauth_scopes" -type "list" -default "allatclaims" -validateSet "allatclaims","openid"
+$name                           = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
+$state                          = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent","present"
+$endpoints                      = Get-AnsibleParam -obj $params -name "endpoints" -type "list" -failifempty $true
+$claimsrules                    = Get-AnsibleParam -obj $params -name "claimsrules" -type "str" -failifempty $true
+$oauthClientType                = Get-AnsibleParam -obj $params -name "oauth_clients_type" -type "str" -default "Public" -validateSet "public", "confidential"
+$scopes                         = Get-AnsibleParam -obj $params -name "oauth_scopes" -type "list" -default "allatclaims" -validateSet "allatclaims","openid"
 # TODO: Add documentation for the options below
-$signatureAlgorithm = Get-AnsibleParam -obj $params -name "signature_algorithm" -type "str" -default "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256" -validateSet "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256","http://www.w3.org/2000/09/xmldsig#rsa-sha1"
-$notBeforeSkew      = Get-AnsibleParam -obj $params -name "not_before_skew" -type "int"
-$type               = Get-AnsibleParam -obj $params -name "type" -type "str" -default "saml" -validateSet "saml", "oauth"
+$signatureAlgorithm             = Get-AnsibleParam -obj $params -name "signature_algorithm" -type "str" -default "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256" -validateSet "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256","http://www.w3.org/2000/09/xmldsig#rsa-sha1"
+$notBeforeSkew                  = Get-AnsibleParam -obj $params -name "not_before_skew" -type "int" -default 0 -validateSet $skewRange
+$tokenLifetime                  = Get-AnsibleParam -obj $params -name "token_lifetime" -type "int" -default 0
+$claimsProviderName             = Get-AnsibleParam -obj $params -name "claims_provider_name" -type "list"
+$encryptClaims                  = Get-AnsibleParam -obj $params -name "encrypt_claims" -type "bool" -default $true
+$refreshTokenProtectionEnabled  = Get-AnsibleParam -obj $params -name "refresh_token_protection_enabled" -type "bool" -default $true
+$type                           = Get-AnsibleParam -obj $params -name "type" -type "str" -default "saml" -validateSet "saml", "oauth"
 
 $beforeValue = Get-AdfsRelyingPartyTrust -name $name
 if($type -eq "oauth"){
@@ -46,6 +51,18 @@ if($state -eq "present" -and $null -eq $beforeValue) {
         if($null -ne $notBeforeSkew){
             $parameters.NotBeforeSkew = $notBeforeSkew
         }
+        if($null -ne $tokenLifetime){
+            $parameters.TokenLifetime = $tokenLifetime
+        }
+        if($null -ne $claimsProviderName){
+            $parameters.ClaimsProviderName = $claimsProviderName
+        }
+        if($null -ne $encryptClaims){
+            $parameters.EncryptClaims = $encryptClaims
+        }
+        if($null -ne $refreshTokenProtectionEnabled){
+            $parameters.RefreshTokenProtectionEnabled = $refreshTokenProtectionEnabled
+        }
 
         Add-AdfsRelyingPartyTrust @parameters
     }
@@ -53,8 +70,12 @@ if($state -eq "present" -and $null -eq $beforeValue) {
 
 } elseif($state -eq "present" -and $null -ne $beforeValue) {
     if($($beforeValue.IssuanceTransformRules -replace "\r\n", "") -ne $claimsrules `
-        -or ($beforeValue.signatureAlgorithm -ne $signatureAlgorithm) `
-        -or ($beforeValue.NotBeforeSkew -ne $notBeforeSkew)){
+        -or ($beforeValue.SignatureAlgorithm -ne $signatureAlgorithm) `
+        -or ($beforeValue.EncryptClaims -ne $encryptClaims) `
+        -or ($beforeValue.RefreshTokenProtectionEnabled -ne $refreshTokenProtectionEnabled) `
+        -or (($null -ne $claimsProviderName -and (Compare-Object -ReferenceObject $claimsProviderName -DifferenceObject $beforeValue.ClaimsProviderName))) `
+        -or ($beforeValue.NotBeforeSkew -ne $notBeforeSkew)`
+        -or ($beforeValue.TokenLifetime -ne $tokenLifetime)){
         if(-not $checkMode) {
             $parameters = @{
                 TargetIdentifier        = $name
@@ -63,6 +84,18 @@ if($state -eq "present" -and $null -eq $beforeValue) {
             }
             if($null -ne $notBeforeSkew){
                 $parameters.NotBeforeSkew = $notBeforeSkew
+            }
+            if($null -ne $tokenLifetime){
+                $parameters.TokenLifetime = $tokenLifetime
+            }
+            if($null -ne $claimsProviderName){
+                $parameters.ClaimsProviderName = $claimsProviderName
+            }
+            if($null -ne $encryptClaims){
+                $parameters.EncryptClaims = $encryptClaims
+            }
+            if($null -ne $refreshTokenProtectionEnabled){
+                $parameters.RefreshTokenProtectionEnabled = $refreshTokenProtectionEnabled
             }
 
             Set-AdfsRelyingPartyTrust @parameters
@@ -122,7 +155,7 @@ if($type -eq "oauth" -and $state -eq "present"){
     }
     else{
         # Check if client needs to be changed
-        if(-not (($beforeValueClient.name -eq $name) -and ($beforeValueClient.RedirectUri -eq $redirectUrl))){
+        if(($beforeValueClient.name -ne $name) -or $null -ne (Compare-Object -ReferenceObject $redirectUrl -DifferenceObject $beforeValueClient.RedirectUri)){
             $parameters = @{
                 ClientId        = $name
                 TargetName      = $name
@@ -142,6 +175,7 @@ if($type -eq "oauth" -and $state -eq "present"){
     }
     elseif($null -ne (Compare-Object -ReferenceObject $scopes -DifferenceObject $beforeValueAppPermission.ScopeNames)){
         Set-AdfsApplicationPermission -TargetIdentifier $beforeValueAppPermission.ObjectIdentifier -ScopeNames $scopes
+        $result.changed = $true
     }
 
 }
